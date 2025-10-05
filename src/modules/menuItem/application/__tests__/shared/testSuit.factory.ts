a289508e6fa84b4fce88aa9d4d8a540779721753
Joanes de Jesus Nunes Junior <64674822+JoanesJr@users.noreply.github.com>
2025-10-04 22:01:30 -0300

    refactor: Corrigindo TestSuitFactory in menuItem module

--- src/modules/menuItem/application/__tests__/shared/testSuit.factory.ts
+++ src/modules/menuItem/application/__tests__/shared/testSuit.factory.ts
@@ -1,7 +1,9 @@
 import { InMemoryMenuItemRepository } from "@/modules/menuItem/adapters/outbound/inMemory/InMemoryMenuItemRepository"
+import { PrismaMenuItemRepository } from "@/modules/menuItem/adapters/outbound/prisma/PrismaMenuItem.repository";
 import { IMenuItemReader } from "@/modules/menuItem/ports/IMenuItemReader";
 import { IMenuItemWriter } from "@/modules/menuItem/ports/IMenuItemWriter";
 import { InMemoryRestaurantRepository } from "@/modules/restaurant/adapters/outbound/inMemory/InMemoryRestaurantRepository"
+import { PrismaRestaurantRepository } from "@/modules/restaurant/adapters/outbound/prisma/PrismaRestaurant.repository";
 import { IRestaurantReader } from "@/modules/restaurant/ports/IRestaurantReader";
 import { IRestaurantWriter } from "@/modules/restaurant/ports/IRestaurantWriter";
 
@@ -22,8 +24,8 @@ export class TestSuitFactory {
 
     static createPrisma(): Deps {
         return {
-            restaurantRepo: new InMemoryRestaurantRepository(),
-            menuItemRepo: new InMemoryMenuItemRepository()
+            restaurantRepo: new PrismaRestaurantRepository(),
+            menuItemRepo: new PrismaMenuItemRepository()
         }
     }
 }
